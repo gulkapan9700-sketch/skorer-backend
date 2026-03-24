34963490d3b272d41c205033e3a7fc23576b9aea
gulkapan9700-sketch <gulkapan9700@gmail.com>
2026-03-24 15:08:53 +0300

    Update footballController.js

--- controllers/footballController.js
+++ controllers/footballController.js
@@ -1,20 +1,15 @@
-
 import fetch from 'node-fetch';
-const API = 'https://v3.football.api-sports.io';
+
+const API = 'https://api-football-v1.p.rapidapi.com/v3';
 const KEY = process.env.RAPID_API_KEY;
 
 async function apiCall(url) {
-  const r = await fetch(url, { headers: { 'x-apisports-key': KEY }});
+  const r = await fetch(url, {
+    headers: {
+      'x-rapidapi-key': KEY,
+      'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+    }
+  });
   const j = await r.json();
   return j.response ?? [];
 }
-
-export const getToday = async (req, res) => {
-  const today = new Date().toISOString().split('T')[0];
-  res.json(await apiCall(`${API}/fixtures?date=${today}`));
-};
-
-export const getLive = async (req, res) => res.json(await apiCall(`${API}/fixtures?live=all`));
-export const getFixture = async (req, res) => res.json(await apiCall(`${API}/fixtures?id=${req.params.id}`));
-export const getOdds = async (req, res) => res.json(await apiCall(`${API}/odds?fixture=${req.params.id}&bookmaker=1`));
-export const getPrediction = async (req, res) => res.json(await apiCall(`${API}/predictions?fixture=${req.params.id}`));
